29754707e46e7533029a2e3e8520ae0ad7c115b7
Luis García Adán <luisgadan88@gmail.com>
2024-07-08 20:03:22 +0200

    Delete keccak.sol

--- keccak.sol
+++
@@ -1,8 +0,0 @@
-pragma solidity ^0.8.0;
-
-contract HashFunction {
-    // Function to hash a string input
-    function hashString(string memory input) public pure returns (bytes32) {
-        return keccak256(abi.encodePacked(input));
-    }
-}
